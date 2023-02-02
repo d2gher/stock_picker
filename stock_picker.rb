0868fd10cc47c14dbd16f3148c14d198d7324f8b
d2gher <d2gher@gmail.com>
2023-02-02 13:20:16 +0200

    Make logic more concise

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,6 +1,5 @@
 def stock_picker(prices)
-  day_to_buy = nil
-  day_to_sell = nil
+  days = []
   highest_profit = 0
 
   prices.each_with_index do |price, index|
@@ -8,13 +7,13 @@ def stock_picker(prices)
     next unless highest_day
 
     if highest_day - price > highest_profit
-      day_to_buy = index
-      day_to_sell = prices[(index + 1)..].find_index(highest_day) + index + 1
+      days = [index, prices[(index + 1)..].find_index(highest_day) + index + 1]
       highest_profit = highest_day - price
     end
     next
   end
-  [day_to_buy, day_to_sell]
+  days
 end
 
 p stock_picker([17, 3, 6, 9, 15, 8, 6, 1, 10])
+# => [1, 4]
